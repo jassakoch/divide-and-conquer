636d6d5c5bf07b2bd1197c106c8302596bd62fc9
paulina koch <pjassakoch@gmail.com>
2025-04-22 21:10:22 -0400

    Inital commit, has tailwind and vitest installed

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-blue-600">Tailwind is working!</h1>
+    </>
+  )
+}
+
+export default App
